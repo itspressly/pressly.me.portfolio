69d579e90ef836e83a7f8d6e9990c65b2af2f43c
it'smepressly <130832755+itspressly@users.noreply.github.com>
2024-10-12 17:08:02 +0200

    first commit

--- index.js
+++ index.js
@@ -4,26 +4,29 @@ window.onload = function() {
 
 
 const projects = [
+    /*
     {
         title: "Image Reader",
         description: "Developed a secure web application for reading and describing the images you output to it.",
         technologies: "HTML, CSS, JavaScript",
         link: "https://yourlink.com/image-reader" // Add a link if you have one
     },
+    
     {
         title: "Weather application",
         description: "Developed an app in Flutter that can predict the weather using a weather API.",
         technologies: "Dart, Flutter, APIs",
         link: "https://yourlink.com/weather-app" // Add a link if you have one
     },
-
+    */
+    /*
     {
         title: "Iron Man Jarvis",
         description: "Developed an AI assistant similar to Iron Man's Jarvis. The assistant performs tasks, talks back automatically, and supports both PC and mobile platforms.",
         technologies: " python, APIs, Voice Recognition, VS code terminial",
         link: "https://yourlink.com/weather-app" // Add a link if you have one
     },
-    
+    */
 ];
 
 const projectList = document.getElementById("projectList");
